e234d82f0f41a52279ae95dc47b4a8f15884acc9
Sulabh Shrestha <sulabh@fusemachines.com>
2017-06-09 10:25:03 +0545

    finished last exercise

--- learnyounode/httpjsonapiserver.js
+++ learnyounode/httpjsonapiserver.js
@@ -9,11 +9,31 @@ var server = http.createServer(function callback(request, response){
 	if(request.method !== 'GET')
 		return console.log('Please send an appropriate GET request!');
 
-	
-	if(request.method === 'GET') 
-		var urlProperties = url.parse(request.url, true);
-		if(urlProperties.pathname !== '/api/parsetime')
-			return console.log
+	// parsing the url properties
+	var urlObj = url.parse(request.url, true);
+
+	var path = urlObj.pathname;
+	// If the path is not the required one
+	if(path !== '/api/parsetime' && path!== '/api/unixtime')
+		return console.log('Wrong pathname!', urlObj.pathname);
+
+	// setting proper type for the response
+	response.writeHead(200, { 'Content-Type': 'application/json' });
+	var date = new Date(urlObj.query.iso);
+
+	if(path==='/api/parsetime')
+		response.write(JSON.stringify({
+			"hour" : date.getHours(),
+			"minute" : date.getMinutes(),
+			"second" : date.getSeconds()
+		}));
+
+	if(path==='/api/unixtime')
+		response.write(JSON.stringify({
+			"unixtime" : date.getTime(),
+		}));
+
+	response.end();
 })
 
 server.listen(+port);
--- learnyounode/test.js
+++ learnyounode/test.js
@@ -1,25 +1,11 @@
 var url = require('url');
 var http = require('http');
-
-var port = process.argv[2];	// port is the 1st argument
-
-console.log(Date().toISOString('2013-08-10T12:10:15.474Z'));
-
-var server = http.createServer(function callback(request, response){
-	// If the method is not GET
-	if(request.method !== 'GET')
-		return console.log('Please send an appropriate GET request!');
-	
-	// Parsing and acquiring various url properties
-	var urlObj = url.parse(request.url, true);
-
-	// checking if pathname is the required one
-	if(urlObj.pathname !== '/api/parsetime')
-		return console.log('Wrong pathname!');
-
-
-	res.writeHead(200, { 'Content-Type': 'application/json' }) 
-	console.log(urlProperties.query.iso);
-})
-
-server.listen(+port);
+var date = new Date('2013-08-10T12:10:15.474Z');
+console.log(date)
+var response = {
+			"hour" : date.getUTCHours(),
+			"minute" : date.getUTCMinutes(),
+			"second" : date.getUTCSeconds()
+		};
+console.log(JSON.stringify(response));
+console.log(date.getTime());
